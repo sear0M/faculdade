4eae792c4bb75aa16031424bc5ec11ad73dda60a
searoM <pedro.moraes23032005@gmail.com>
2024-03-08 23:58:33 -0300

    Update f009.py
    change code

--- f009.py
+++ f009.py
@@ -1,12 +1,12 @@
 nota = int(input('digite sua nota: '))
 
-if nota >= 0 and nota <5:
+if nota <= 5:
     print('conceito D')
-elif nota == 5 and nota == 6:
+elif nota == 5 or nota == 6:
     print('conceito C')
-elif nota == 7 and nota == 8:
+elif nota == 7 or nota == 8:
     print('conceito B')
-elif nota == 9 and nota == 10:
+elif nota == 9 or nota == 10:
     print('conceito A')
 else:
     print('nota invalida')
