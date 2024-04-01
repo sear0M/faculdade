6d865202b538c3aa1c906ef4719bb6d3d2631fae
searoM <pedro.moraes23032005@gmail.com>
2024-04-01 01:12:32 -0300

    Update f014.py

--- f014.py
+++ f014.py
@@ -4,7 +4,7 @@ if numero < 0:
     print("Erro: Não é possível calcular o fatorial de um número negativo.")
 else:
     fatorial = 1
-    # Calcula o fatorial do número
+    
     for i in range(1, numero + 1):
         fatorial *= i
     
