309572e1984722e07966f435abffa63b98192404
searoM <pedro.moraes23032005@gmail.com>
2024-04-26 16:35:37 -0300

    Update forca.py

--- forca.py
+++ forca.py
@@ -1,3 +1,4 @@
+#Pedro Moraes Carvalho   #Pedro Enrico Oliveira Santos
 import random
 
 with open("br-sem-acentos.txt", "r") as file:
@@ -11,10 +12,11 @@ letras_certas = []
 letras_erradas = []
 
 tentativas = 6
+venceu = False
 
 print("A palavra tem", len(palavra), "letras.")
 
-while tentativas > 0:
+while tentativas > 0 and not venceu:
     palavra_atual = ""
     for letra in palavra:
         if letra in letras_certas:
@@ -27,13 +29,13 @@ while tentativas > 0:
         print("Letras erradas:", " ".join(letras_erradas))
 
     if "_" not in palavra_atual:
-        print("voce ganhou")
-        tentativas = 0
+        print("Você ganhou!")
+        venceu = True
     else:
         letra = input("Digite uma letra: ").lower()
 
         if letra in letras_certas or letra in letras_erradas:
-            print("Voce ja escolheu essa letra. Tente novamente.")
+            print("Você já escolheu essa letra. Tente novamente.")
             continue
 
         if letra in palavra:
@@ -41,7 +43,7 @@ while tentativas > 0:
         else:
             letras_erradas.append(letra)
             tentativas -= 1
-            print("Letra incorreta. Voce tem", tentativas, "tentativas restantes.")
+            print("Letra incorreta. Você tem", tentativas, "tentativas restantes.")
 
-if tentativas == 0:
-    print("Voce perdeu, a palavra era:", palavra)
+if not venceu:
+    print("Você perdeu, a palavra era:", palavra)
